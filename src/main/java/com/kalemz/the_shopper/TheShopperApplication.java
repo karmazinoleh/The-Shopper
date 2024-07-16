ebe200271d6e0b1546638b2a35d9e127e7f3110e
Oleh Karmazin <154458302+karmazinoleh@users.noreply.github.com>
2024-07-16 19:22:05 +0300

    first commit

---
+++ src/main/java/com/kalemz/the_shopper/TheShopperApplication.java
@@ -0,0 +1,13 @@
+package com.kalemz.the_shopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheShopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheShopperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalemz/the_shopper/TheShopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalemz.the_shopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheShopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
