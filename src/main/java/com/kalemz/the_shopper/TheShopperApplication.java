f118ce51e93d3cd90bd3cfd4761ce3c36a5e05f4
Oleh Karmazin <154458302+karmazinoleh@users.noreply.github.com>
2024-07-17 15:20:55 +0300

    Created some basic packages, files structure, etc.
    Work still in progress on Security part of the application! There is much more to be done. I hope will end this chapter by the end of the week.

--- src/main/java/com/kalemz/the_shopper/TheShopperApplication.java
+++ src/main/java/com/kalemz/the_shopper/TheShopperApplication.java
@@ -2,8 +2,10 @@ package com.kalemz.the_shopper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TheShopperApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/kalemz/the_shopper/security/JwtService.java
@@ -0,0 +1,7 @@
+package com.kalemz.the_shopper.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+}
