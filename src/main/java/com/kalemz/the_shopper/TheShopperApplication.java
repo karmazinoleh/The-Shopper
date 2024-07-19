157a48ed89ec990fc41fcd78db108e0713d71ad1
Oleh Karmazin <154458302+karmazinoleh@users.noreply.github.com>
2024-07-19 19:09:57 +0300

    minor update
    I resolved the problem with the database, but I still have another issue with sending POST requests. 
    Sending this to Postman and getting a 403 error :/
    http://localhost:8088/api/v1/auth/register

--- src/main/java/com/kalemz/the_shopper/TheShopperApplication.java
+++ src/main/java/com/kalemz/the_shopper/TheShopperApplication.java
@@ -1,7 +1,11 @@
 package com.kalemz.the_shopper;
 
+import com.kalemz.the_shopper.role.Role;
+import com.kalemz.the_shopper.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -14,4 +18,13 @@ public class TheShopperApplication {
 		SpringApplication.run(TheShopperApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+
+	}
 }
