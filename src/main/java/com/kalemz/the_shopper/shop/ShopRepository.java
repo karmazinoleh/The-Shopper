dba8d66610afe1628f3f2254d42463e8d15603c9
kalemz <kalemzwork@gmail.com>
2024-07-28 15:56:01 +0300

    Models for Shop

---
+++ src/main/java/com/kalemz/the_shopper/shop/ShopRepository.java
@@ -0,0 +1,10 @@
+package com.kalemz.the_shopper.shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+
+}
---
+++ src/main/java/com/kalemz/the_shopper/shop/ShopRequest.java
@@ -0,0 +1,8 @@
+package com.kalemz.the_shopper.shop;
+
+import lombok.Data;
+
+@Data
+public class ShopRequest {
+        private String name;
+}
